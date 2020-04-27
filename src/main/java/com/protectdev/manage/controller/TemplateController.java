0304178de2693b9a455b6aa9bd667d87a3458d6f
constructor-md <826118236@qq.com>
2020-04-28 01:35:36 +0800

    constructor-md

--- src/main/java/com/protectdev/manage/controller/TemplateController.java
+++ src/main/java/com/protectdev/manage/controller/TemplateController.java
@@ -3,7 +3,14 @@ package com.protectdev.manage.controller;
 
 import com.protectdev.manage.annotation.PermissionCheck;
 import com.protectdev.manage.mapper.DeviceMapper;
+import com.protectdev.manage.mapper.FieldMapper;
+import com.protectdev.manage.mapper.SZoneMapper;
+import com.protectdev.manage.mapper.TemplateMapper;
+import com.protectdev.manage.pojo.Field;
+import com.protectdev.manage.pojo.SZone;
+import com.protectdev.manage.pojo.Template;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,23 +27,64 @@ public class TemplateController {
     @Resource
     private DeviceMapper deviceMapper;
 
+    @Resource
+    private TemplateMapper templateMapper;
+
+    @Resource
+    private SZoneMapper sZoneMapper;
+
+    @Resource
+    private FieldMapper fieldMapper;
+
 
     @RequestMapping("tem/add")
     @ResponseBody
     @PermissionCheck("temAdd")
-    public String temAdd(){
+    public String temAdd(@RequestBody Template template){
 
 
+        int addTemplate = templateMapper.addTemplate(template);
 
+        if (addTemplate != 0){
+            return "ok";
+        }
+
+
+        return "";
+    }
+
+    @RequestMapping("sZone/add")
+    @ResponseBody
+    @PermissionCheck("temAdd")
+    public String sZoneAdd(@RequestBody SZone sZone){
+
+
+        int addSZone = sZoneMapper.addSZone(sZone);
+
+        if (addSZone != 0){
+            return "ok";
+        }
 
 
         return "";
     }
 
 
+    @RequestMapping("field/add")
+    @ResponseBody
+    @PermissionCheck("temAdd")
+    public String fieldAdd(@RequestBody Field field){
 
 
+        int addField = fieldMapper.addField(field);
 
+        if (addField != 0){
+            return "ok";
+        }
+
+
+        return "";
+    }
 
 
 }
